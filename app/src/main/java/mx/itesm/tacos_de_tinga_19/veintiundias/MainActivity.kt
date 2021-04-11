ad338257e40af6a6c1500413afa51a4b60dd3a8a
Ivan <ivanhonc@live.com.mx>
2021-04-11 13:16:34 -0500

    Added base files

---
+++ app/src/main/java/mx/itesm/tacos_de_tinga_19/veintiundias/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.itesm.tacos_de_tinga_19.veintiundias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
